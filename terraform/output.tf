b079dc8de79ed7b3304950f022ea103e5a417fe0
Gauri Mandlik <gauri.mandlik@indiamart.com>
2026-06-14 13:56:56 +0530

    Name correction

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
 output "aws_vpc_id" {
-  value = aws_vpc.proj2_vpc.id
+  value = aws_vpc.proj3_vpc.id
 }
 
 output "aws_subnet_1_id" {
-  value = aws_subnet.proj2_subnet_1.id
+  value = aws_subnet.proj3_subnet_1.id
 }
 
 output "aws_subnet_2_id" {
-  value = aws_subnet.proj2_subnet_2.id
+  value = aws_subnet.proj3_subnet_2.id
 }
